f3ab2c1bdd980b3c41c73c6576d0185dc79a5bff
devlou-rens <lourensgacias00@gmail.com.github.com>
2026-03-06 23:38:36 +0800

    Delete File

--- check_models.py
+++
@@ -1,9 +0,0 @@
-import google.genai as genai
-
-API_KEY = input("AIzaSyBBMB2qg1Xnfhl56dJN8dfbagGC12ZptS0").strip()
-genai.configure(api_key=API_KEY)
-
-print("\nAvailable models that support generateContent:\n")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(" -", m.name)
